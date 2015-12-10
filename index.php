145dd4744f10fcd0f4adfe12bd52b018f5d8b3b1
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-12-11 00:04:18 +0530

    index final

--- index.php
+++ index.php
@@ -42,7 +42,11 @@
 		border-color:red;
 
 	}
-</style>
+	.auto-style3 {
+		border-style: solid;
+		border-width: 1px;
+	}
+	</style>
 
   </head>
 <!-- NAVBAR
@@ -77,18 +81,54 @@
 		<tr id="tr-1">
 			<td id="tr-1-poster" class="auto-style2">Poster1</td>
 			<td id="tr-1-details" class="auto-style2">details1</td>
-			<td id="tr-1-reviews" class="auto-style2">reviews 1</td>
+			<td id="tr-1-reviews" class="auto-style2">Movie 1 Reviews
+			<table style="width: 100%">
+				<tr id="r-1-1">
+					<td id="d-1-1" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-1-2" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-1-3" class="auto-style3">&nbsp;</td>
+				</tr>
+			</table>
+			</td>
 		</tr>
 		<tr id="tr-2">
 			<td id="tr-2-poster" class="auto-style2">Poster2</td>
 			<td id="tr-2-details" class="auto-style2">details2</td>
+			<td id="tr-2-reviews" class="auto-style2">Movie 2 Reviews
+			<table style="width: 100%">
+				<tr id="r-2-1">
+					<td id="d-2-1" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-2-2" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-2-3" class="auto-style3">&nbsp;</td>
+				</tr>
+			</table>
 
-			<td id="tr-2-reviews" class="auto-style2">reviews 2</td>
+			</td>
 		</tr>
 		<tr id="tr-3">
 			<td id="tr-3-poster" class="auto-style2">&nbsp;</td>
 			<td id="tr-3-details" class="auto-style2">&nbsp;</td>
-			<td id="tr-3-reviews" class="auto-style2">Title: The Last One: Part 2| Year: 2004| Rated: TV-PG| Genre: Comedy, Romance| Runtime: 22 min| IMDB Rating: 9.5| IMDB Votes: 2783</td>
+			<td id="tr-3-reviews" class="auto-style2">Movie 2 Reviews
+			<table style="width: 100%">
+				<tr id="r-3-1">
+					<td id="d-3-1" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-3-2" class="auto-style3">&nbsp;</td>
+				</tr>
+				<tr>
+					<td id="d-3-3" class="auto-style3">&nbsp;</td>
+				</tr>
+			</table>
+</td>
 		</tr>
 	</table>
         </div>
@@ -107,15 +147,15 @@ $last = $_GET["actorLast"];
 $name = $first."%20".$last;
 echo $name.'</br>';
 $url = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=".$name;
-echo $url.'</br>';
+//echo $url.'</br>';
 $str = file_get_contents($url);
 $content = file_get_contents($url);
 $json = json_decode($content, true);
 $names = $json['name_popular'];
 $id = $names[0]['id'];
-echo $id.'</br>';
+//echo $id.'</br>';
 
-$url = "http://www.imdb.com/filmosearch?sort=user_rating&amp;explore=title_type&amp;role=".$id."&amp;ref_=nm_flmg_shw_3";
+$url = "http://www.imdb.com/filmosearch?sort=user_rating&amp;explore=title_type&amp;role=".$id."&ref_=filmo_ref_typ&sort=user_rating,desc&mode=detail&page=1&title_type=movie";
 $home = file_get_contents($url);
 echo "<div id=\"resultDiv\">".$home."</div>";
 
@@ -128,7 +168,15 @@ $i = 0;
 ?>
 
 </div>
+<div id="temp-review-1">
 
+</div>
+<div id="temp-review-2">
+
+</div>
+<div id="temp-review-3">
+
+</div>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -143,7 +191,9 @@ $i = 0;
  <script type="text/javascript">
     
 	
-	var div = document.getElementsByClassName("lister-item-image");
+	var div = document.getElementsByClassName("lister-item-image");        
+        
+
 	
 /*	var divTable = document.getElementById("results");
 	divTable.style.visibility = "visible";
@@ -152,6 +202,22 @@ $i = 0;
 var count =0;
     for(var i=0;i<3;i++){
     var movieId = div[i].getAttribute("data-tconst");
+            if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("temp-review-"+(i+1)).innerHTML = xmlhttp.responseText;
+                populateReviews(i+1);
+            }
+        };
+        xmlhttp.open("GET","reviewFunc.php?movieId="+movieId,false);
+        xmlhttp.send();
+
         $.getJSON("http://www.omdbapi.com/?i="+movieId, function(result){
             var thumb = result.Poster;
             var title = result.Title;
@@ -182,13 +248,38 @@ var count =0;
 	}
 	
 	$('#resultDiv').remove();
+	
 
 /*$( document ).ready(function() {
 
 $('#resultDiv').remove();
     console.log( "ready!" );
 });
-*/  </script>
+*/ 
+function populateReviews(k){
+	var div = document.getElementById("tn15content");
+	var reviews = div.getElementsByTagName("p");
+	var divs = div.getElementsByTagName("div");
+	var count = 0;
+	var usefulDivs = [];
+	for(var i=0; i<3; i++){
+		var total = divs[i*2].getElementsByTagName("small")[0].innerHTML;
+		var totalDisplay = total.split(":")[0];
+		var title = divs[i*2].getElementsByTagName("h2")[0].innerHTML;
+		var user = divs[i*2].getElementsByTagName("a")[1].innerHTML;
+		var loc = divs[i*2].getElementsByTagName("small")[1].innerHTML;
+		var date = divs[i*2].getElementsByTagName("small")[2].innerHTML;
+		var review = reviews[i].innerHTML;
+		var display = "<b>"+title+"</b></br>("+user+" "+loc+")</br>"+review;
+	//	alert("d-"+k+"-"+(i+1));
+		var field = document.getElementById("d-"+k+"-"+(i+1));
+		field.innerHTML = display;
+		//alert(review);
+	}
+	$('#temp-review-'+k).remove();
+}
+
+ </script>
 
   </body>
 </html>
